f40134b9f614cbfe1cab280c903ff1e18ce1683c
Crupette <jonsantmyer@protonmail.com>
2019-04-29 17:38:48 -0400

    Added longopts, --help and --version

--- src/main.c
+++ src/main.c
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_BUGFIX 1
 
 float iterations = 100;
 int startzoom = 1;
@@ -23,6 +28,8 @@ float autozoom = 0;
 float granularity = 2;
 char smoothing = 1;
 
+int helpFlag = 0, versionFlag = 0;
+
 char *colors[] = {
 	"\033[30m.",
 	"\033[30m*",
@@ -37,6 +44,42 @@ char *colors[] = {
 	"\033[91m#",
 	"\033[97m#",
 };
+
+static struct option long_options[] = {
+	{"help", no_argument, &helpFlag, 1},
+	{"version", no_argument, &versionFlag, 1},
+	{"width", required_argument, 0, 'w'},
+	{"height", required_argument, 0, 'h'},
+	{"panx", required_argument, 0, 'x'},
+	{"pany", required_argument, 0, 'y'},
+	{"iterations", required_argument, 0, 'i'},
+	{"zoom", required_argument, 0, 'z'},
+	{"delay", required_argument, 0, 'd'},
+	{"autoincrement", required_argument, 0, 'a'},
+	{"autozoom", required_argument, 0, 'A'},
+	{"granularity", required_argument, 0, 'g'},
+	{"nosmooth", no_argument, (int*)&smoothing, 1},
+	{NULL, 0, NULL, 0}
+};
+
+void help(void){
+	printf("Usage: mandel [OPTION]\n");
+	printf("Prints a mandel brot zoom to the terminal with variable height and optional zooming\n\n");
+	printf("Options:\n");
+	printf("  -w --width:      Width to render the set.\n");
+	printf("  -h --height:     Height to render the set.\n");
+	printf("  -x --panx:       X coordinate to zoom into. (float)\n");
+	printf("  -y --pany:       Y coordinate to zoom into. (float)\n");
+	printf("  -i --iterations: Number of iterations (detail)\n");
+	printf("  -z --zoom:       Ammount of zoom to start with.\n");
+	printf("  -d --delay:      Number of milliseconds to delay each frame.\n");
+	printf("  -a --autoincrement: Ammount of detail to add to the iterations each frame.\n");
+	printf("  -A --autozoom:   Ammount of zoom to add to the zoom factor each frame.\n");
+	printf("  -g --granularity:   Inverse ammount of smoothing to preform.\n");
+	printf("  -s --nosmooth:   Do not smooth the frame.\n");
+	printf("     --help:       Shows this imformation.\n");
+	printf("     --version:    Shows the version number.\n");
+}
 
 int mandel(double x, double y){
 	double realComp = x;
@@ -57,9 +100,13 @@ int mandel(double x, double y){
 
 int parseargs(int argc, char **argv){
 	int c;
+	int option_index = 0;
 	opterr = 0;
-	while((c = getopt(argc, argv, "w:h:x:y:i:z:d:a:A:g:s:")) != -1){
+	while((c = getopt_long(argc, argv, "w:h:x:y:i:z:d:a:A:g:s:", long_options, &option_index)) != -1){
 		switch(c){
+			case '0':
+
+			break;
 			case 'w':
 				termWidth = atoi(optarg);
 			break;
@@ -91,7 +138,7 @@ int parseargs(int argc, char **argv){
 				granularity = atof(optarg);
 			break;
 			case 's':
-				smoothing = atoi(optarg);
+				smoothing = 0;
 			break;
 			case '?':
 				if(optopt == 'w' || optopt == 'h' || optopt == 'i' || optopt == 'z'){
@@ -103,9 +150,15 @@ int parseargs(int argc, char **argv){
 				}
 				return 1;
 			break;
-			default:
-				abort();
 		}
+	}
+	if(helpFlag){
+		help();
+		return 1;
+	}
+	if(versionFlag){
+		printf("asciimandelbrot v. %i.%i.%i\n", VERSION_MAJOR, VERSION_MINOR, VERSION_BUGFIX);
+		return 1;
 	}
 	return 0;
 }
